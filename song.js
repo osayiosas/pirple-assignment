d97df03988368af705c7882d5a87d9547bc07a26
Idiaghe Osaigbovo <idiagheosaigbovoosas@gmail.com>
2020-11-17 15:53:44 +0100

    let me try this formate and see it work

---
+++ song.js
@@ -0,0 +1,26 @@
+// Assigment for DataType  
+
+var artestName = "jazzy"; //this is a string 
+
+var album = {
+    title : "newday",
+    songNumber: "10",
+    hitSong: "command"
+};  // this a object 
+
+var yearRelased = 2017; // this a number 
+
+var songTitle = ["hello", "abt", "yes"]; // this is arrary.   
+
+var albumDuration = "234"; // this is a string.
+
+
+var newAlbum = new Boolean(true); //this is a boolean.
+
+
+console.log(artestName);
+console.log(album);
+console.log(yearRelased);
+console.log(songTitle);
+console.log(albumDuration);
+console.log(newAlbum); // all this datatypes was consonlo
